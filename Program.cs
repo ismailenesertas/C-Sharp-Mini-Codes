6f04a6884aae5cf2cbdfe4539cb623c3dd94ad5c
İsmail Enes ERTAŞ <240502020@ogrenci.artvin.edu.tr>
2025-10-30 11:51:45 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,47 +1,128 @@
-﻿using System;
+﻿using System.Runtime.Serialization.Formatters;
 
-namespace collections
-
-/*Kullanıcıdan 5 tane şehir ismi girmesini isteyiniz. 
-Bu şehirleri liste koleksiyonuna ekleyiniz. 
-Kullanıcının girmiş olduğu şehir isimleri içerisinde Artvin şehri varsa listeyi büyükten küçüğe doğru sıralayınız
-Kullanıcının girmiş olduğu şehir isimleri içerisinde Artvin şehri yoksa küçükten büyüğe doğru sıralayınız. 
-Listenin son halini ekrana yazdırınız*/
+namespace dictinory_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("5 Tane City Giriniz\n");
+            Console.WriteLine("Plaka Sorgulama Uygulaması");
 
-            List<string> sehirler = new List<string>();
-
-            for (int i = 0; i < 5; i++)
+            Dictionary<int, string> plakaSozluk = new Dictionary<int, string>()
             {
-                Console.WriteLine($"{i + 1}. Şehri Giriniz:\n ");
-                string sehir = Console.ReadLine();
-                sehirler.Add(sehir);
-            }
-
-            if (sehirler.Contains("Artvin"))
+                {1, "Adana"},
+                {2, "Adıyaman"},
+                {3, "Afyonkarahisar"},
+                {4, "Ağrı"},
+                {5, "Amasya"},
+                {6, "Ankara"},
+                {7, "Antalya"},
+                {8, "Artvin"},
+                {9, "Aydın"},
+                {10, "Balıkesir"},
+                {11, "Bilecik"},
+                {12, "Bingöl"},
+                {13, "Bitlis"},
+                {14, "Bolu"},
+                {15, "Burdur"},
+                {16, "Bursa"},
+                {17, "Çanakkale"},
+                {18, "Çankırı"},
+                {19, "Çorum"},
+                {20, "Denizli"},
+                {21, "Diyarbakır"},
+                {22, "Edirne"},
+                {23, "Elazığ"},
+                {24, "Erzincan"},
+                {25, "Erzurum"},
+                {26, "Eskişehir"},
+                {27, "Gaziantep"},
+                {28, "Giresun"},
+                {29, "Gümüşhane"},
+                {30, "Hakkari"},
+                {31, "Hatay"},
+                {32, "Isparta"},
+                {33, "Mersin"},
+                {34, "İstanbul"},
+                {35, "İzmir"},
+                {36, "Kars"},
+                {37, "Kastamonu"},
+                {38, "Kayseri"},
+                {39, "Kırklareli"},
+                {40, "Kırşehir"},
+                {41, "Kocaeli"},
+                {42, "Konya"},
+                {43, "Kütahya"},
+                {44, "Malatya"},
+                {45, "Manisa"},
+                {46, "Kahramanmaraş"},
+                {47, "Mardin"},
+                {48, "Muğla"},
+                {49, "Muş"},
+                {50, "Nevşehir"},
+                {51, "Niğde"},
+                {52, "Ordu"},
+                {53, "Rize"},
+                {54, "Sakarya"},
+                {55, "Samsun"},
+                {56, "Siirt"},
+                {57, "Sinop"},
+                {58, "Sivas"},
+                {59, "Tekirdağ"},
+                {60, "Tokat"},
+                {61, "Trabzon"},
+                {62, "Tunceli"},
+                {63, "Şanlıurfa"},
+                {64, "Uşak"},
+                {65, "Van"},
+                {66, "Yozgat"},
+                {67, "Zonguldak"},
+                {68, "Aksaray"},
+                {69, "Bayburt"},
+                {70, "Karaman"},
+                {71, "Kırıkkale"},
+                {72, "Batman"},
+                {73, "Şırnak"},
+                {74, "Bartın"},
+                {75, "Ardahan"},
+                {76, "Iğdır"},
+                {77, "Yalova"},
+                {78, "Karabük"},
+                {79, "Kilis"},
+                {80, "Osmaniye"},
+                {81, "Düzce"}
+        };
+            while (true)
             {
-                sehirler.Sort();
+                Console.Write("Plaka kodunu giriniz (Çıkmak için 'x'e basın): ");
                 
-                
+                string plaka = Console.ReadLine();
+
+                if (plaka == "x" || plaka == "X")
+                {
+                    Console.WriteLine("Uygulamadan çıkılıyor...");
+                    break;
+                }
+
+                bool isNumeric = int.TryParse(plaka, out int plakaKodu);
+                if (isNumeric)
+                {
+                    if (plakaSozluk.TryGetValue(plakaKodu, out string sehirAdi))
+                    {
+                        Console.WriteLine($"\nPlaka kodu {plakaKodu} olan şehir: {sehirAdi}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nGeçersiz plaka kodu. Lütfen 1 ile 81 arasında bir sayı giriniz.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nGeçersiz giriş. Lütfen geçerli bir plaka kodu giriniz.");
+                }
+
 
             }
-            else
-            {
-                sehirler.Sort();
-                sehirler.Reverse();
-            }
-            Console.WriteLine("Sıralanan Şehirler Böyle Sıralandı");
-
-            foreach (var sehir in sehirler)
-            {
-                
-                Console.WriteLine("\n:" + sehir);
-            }
-        }
+    }
     }
 }
